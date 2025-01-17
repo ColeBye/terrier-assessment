614bcb977af8ac421fd722fd10b9c815b678a144
Cole Bye <bye00036@umn.edu>
2025-01-17 15:21:51 -0600

    Controller rakes from db at outputs into test view

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,4 +1,5 @@
 class ScheduleController < ApplicationController
+
   def index
     @work_orders = WorkOrder.all
     @locations = Location.all
@@ -17,10 +18,45 @@ class ScheduleController < ApplicationController
   end
 
   def import
-    file = params[:file]
+    puts("test")
+    work_order_file = params[:work_order_file]
+    technician_file = params[:technician_file]
+    location_file = params[:location_file]
 
-    redirect_to schedule_index_path, alert: "No file selected" unless file
-    redirect_to schedule_index_path, notice: "CSV files only" unless file.content_type == "text/csv"
-    # redirect_to schedule_index_path, notice: "CSV Imported"
+    return redirect_to schedule_index_path, alert: "Missing File/s" unless work_order_file && technician_file && location_file
+    return redirect_to schedule_index_path, notice: "CSV files only" unless work_order_file.content_type == "text/csv" && technician_file.content_type == "text/csv" && location_file.content_type == "text/csv"
+
+    work_order_file = File.open(work_order_file.to_s)
+    csv = CSV.parse(work_order_file, headers: true, col_sep: ";")
+    csv.each do |row|
+      work_order_hash = {}
+      work_order_hash[:id] = row["id"]
+      work_order_hash[:technician] = row["technician_id"]
+      work_order_hash[:location] = row["location_id"]
+      work_order_hash[:date_time] = row["time"]
+      work_order_hash[:duration] = row["duration"]
+      work_order_hash[:price] = row["price"]
+      WorkOrder.create(work_order_hash)
+    end
+    technician_file = File.open(technician_file.to_s)
+    csv = CSV.parse(technician_file, headers: true, col_sep: ";")
+    csv.each do |row|
+      technician_hash = {}
+      technician_hash[:id] = row["id"]
+      technician_hash[:name] = row["name"]
+      Technician.create(technician_hash)
+    end
+    location_file = File.open(location_file.to_s)
+    csv = CSV.parse(location_file, headers: true, col_sep: ";")
+    csv.each do |row|
+      location_hash = {}
+      location_hash[:id] = row["id"]
+      location_hash[:name] = row["name"]
+      location_hash[:city] = row["city"]
+      Location.create(location_hash)
+    end
+
+
+    redirect_to schedule_index_path, notice: "CSV Imported"
   end
 end
