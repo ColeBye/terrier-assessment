5ab79ad568d82f5dd270217ef38c1162578c6809
Cole Bye <bye00036@umn.edu>
2025-01-17 23:25:45 -0600

    Fixed start and end hour assignments

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -7,14 +7,12 @@ class ScheduleController < ApplicationController
       @start_hour = 0
       @end_hour = 0
     else
-      # @start_hour = WorkOrder.order(date_time: :asc).pluck(:date_time).first.hour
-      @start_hour = 0
-      # finish_times = []
-      # @work_orders.each do |order|
-      #   finish_times.append(order.date_time + order.duration)
-      # end
-      # @end_hour = finish_times.max.hour + 1
-      @end_hour = 0
+      @start_hour = WorkOrder.order(date_time: :asc).pluck(:date_time).first.hour
+      finish_times = []
+      @work_orders.each do |order|
+        finish_times.append(order.date_time + order.duration)
+      end
+      @end_hour = finish_times.max.hour + 1
     end
   end
 
