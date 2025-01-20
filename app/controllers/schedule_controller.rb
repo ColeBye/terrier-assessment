ce217f4f9d0a45b4344f547b9415bef99616f4e9
Cole Bye <bye00036@umn.edu>
2025-01-19 23:34:04 -0600

    implemented gaps between events that size according to time

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,6 +1,6 @@
 class ScheduleController < ApplicationController
   def index
-    @work_orders = WorkOrder.all
+    @work_orders = WorkOrder.all.sort_by { |w| w.date_time + (w.duration * 60)}
     @locations = Location.all
     @technicians = Technician.all
     if @work_orders.empty?
@@ -10,9 +10,15 @@ class ScheduleController < ApplicationController
       @start_hour = WorkOrder.order(date_time: :asc).pluck(:date_time).first.hour
       finish_times = []
       @work_orders.each do |order|
-        finish_times.append(order.date_time + order.duration)
+        finish_times.append(order.date_time + (order.duration * 60))
       end
       @end_hour = finish_times.max.hour + 1
+
+
+      max_hour = finish_times.max.time
+      logger.info "MAXXXXXX"
+      logger.info max_hour
+      @end_hour = if max_hour.min == 0 && max_hour.sec == 0 then max_hour.hour else max_hour.hour + 1 end
     end
   end
 
